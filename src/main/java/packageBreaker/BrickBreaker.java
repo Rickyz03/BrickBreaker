7bc66216bf6fc887bbcc1a5358d1d01d0409d98c
Riccardo Stefani <riccardost02@gmail.com>
2024-09-17 12:03:41 +0200

    Introduzione di un file Header coincidente con la licenza, ma senza parentesi, siccome il module RegexpHeader purtroppo non riconosce le parentesi

--- src/main/java/packageBreaker/BrickBreaker.java
+++ src/main/java/packageBreaker/BrickBreaker.java
@@ -1,10 +1,10 @@
-////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 // MIT License
 //
-// Copyright (c) 2024 Riccardo Stefani
+// Copyright c 2024 Riccardo Stefani
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
+// of this software and associated documentation files, the "Software", to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
@@ -20,7 +20,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 package packageBreaker;
 
